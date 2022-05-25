bd1e4f421f27d3714f9aa9e1f2cb226f074bc560
mdshohed <mdshohed170@gmail.com>
2022-05-25 08:27:40 +0600

    products get update

---
+++ README.md
@@ -0,0 +1,12 @@
+## [Github Code link](https://github.com/programming-hero-web-course1/manufacturer-website-server-side-mdshohed)
+## [Live Site link]()
+
+
+## Project Description
+- 
+
+## Project used Feature 
+- 
+
+## Firebase Deploy
+- 
---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u5otf.mongodb.net/?retryWrites=true&w=majority`;
+
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.twtll.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: 'UnAuthorized access' });
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: 'Forbidden access' })
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
+async function run() {
+  try {
+    await client.connect();
+    const productsCollection = client.db('camera_tools').collection('products');
+
+    app.get('/products', async(req, res)=>{
+      const products = await productsCollection.find().toArray(); 
+      res.send(products); 
+    })
+  }
+  finally{
+
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Hello From photography-camera-tools-site!')
+})
+
+app.listen(port, () => {
+  console.log(`photography-camera-tools listening on port ${port}`)
+})
