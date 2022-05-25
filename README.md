1e6506b5b246367899acf56bfca155714309fa54
mdshohed <mdshohed170@gmail.com>
2022-05-25 20:13:17 +0600

    order update

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## [Github Code link](https://github.com/programming-hero-web-course1/manufacturer-website-server-side-mdshohed)
-## [Live Site link]()
+## [Live Site link](https://damp-beach-74920.herokuapp.com/)
 
 
 ## Project Description
--- index.js
+++ index.js
@@ -12,8 +12,6 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u5otf.mongodb.net/?retryWrites=true&w=majority`;
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.twtll.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 function verifyJWT(req, res, next) {
@@ -34,11 +32,59 @@ function verifyJWT(req, res, next) {
 async function run() {
   try {
     await client.connect();
-    const productsCollection = client.db('camera_tools').collection('products');
+    const toolsCollection = client.db('camera_tools').collection('tools');
+    const userCollection = client.db('camera_tools').collection('users');
+    const orderCollection = client.db('camera_tools').collection('orders');
 
-    app.get('/products', async(req, res)=>{
-      const products = await productsCollection.find().toArray(); 
-      res.send(products); 
+    app.put('/user/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      res.send({ result, token });
+    })
+
+    app.get('/tools', async(req, res)=>{
+      const tools = await toolsCollection.find().toArray(); 
+      res.send(tools); 
+    })
+
+    app.get('/tool/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)}; 
+      const tool = await toolsCollection.findOne(query); 
+      res.send(tool); 
+    })
+
+    app.get('/order', async(req, res)=>{
+
+    })
+
+    app.post('/order',async(req, res)=>{
+      const order = req.body; 
+      const result = await orderCollection.insertOne(order); 
+      res.send({result, success: true}); 
+      // const currentQuentity = order.quantity;
+      // const filter = {toolsName: order.toolsName}
+      // const exists = await orderCollection.findOne(filter);
+      // if(exists) {
+      //   order.quantity = parseInt(exists.quantity) + parseInt(currentQuentity);
+      //   const updateDoc = {
+      //     $set: {quantity: order.quantity }
+      //   }
+      //   console.log(order);
+      //   const result = await orderCollection.updateOne(filter, updateDoc); 
+      //   return res.send({success: true, order})
+      // }
+      // else {
+      //   const result = await orderCollection.insertOne(order); 
+      //   return res.send({result, success: true}); 
+      // }
     })
   }
   finally{
